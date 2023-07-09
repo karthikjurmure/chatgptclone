2822082065f2331217d3e0a7ae421e7440d738e4
Jurmure Sai karthik <122858588+karthikjurmure@users.noreply.github.com>
2023-07-09 22:13:12 +0530

    Update script.js

--- cloneofchatgpt/script.js
+++ cloneofchatgpt/script.js
@@ -5,7 +5,7 @@ const themeButton = document.querySelector("#theme-btn");
 const deleteButton = document.querySelector("#delete-btn");
 
 let userText = null;
-//const API_KEY = "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"; // Paste your API key here
+const API_KEY =key(); // Paste your API key here
 
 const loadDataFromLocalstorage = () => {
     // Load saved chats and theme from local storage and apply/add on the page
